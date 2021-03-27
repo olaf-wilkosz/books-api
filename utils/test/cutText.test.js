8ff942767dcde068283ef67453d1cd09469f01b0
Olaf <67223543+olaf-wilkosz@users.noreply.github.com>
2021-03-27 20:46:39 +0100

    Modify test script and add first test to cutText testing file

---
+++ utils/test/cutText.test.js
@@ -0,0 +1,16 @@
+const cutText = require('../cutText.js');
+const expect = require('chai').expect;
+
+describe('CutText', () => {
+
+  it('should return an error if "content" arg is not a string', () => {
+    if (typeof content !== 'string') return 'Error';
+
+    expect(cutText(undefined, 20)).to.equal('Error');
+    expect(cutText(12, 20)).to.equal('Error');
+    expect(cutText({}, 20)).to.equal('Error');
+    expect(cutText([], 20)).to.equal('Error');
+    expect(cutText(function () { }, 20)).to.equal('Error');
+  });
+
+});
